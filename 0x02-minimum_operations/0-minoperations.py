1f1ea82df9e88e33d6f75379db3ab6461e35692a
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-12-12 15:55:17 +0100

    Cleared pycodestyle errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Defines a function minOperations"""
 
+
 def minOperations(n):
     """Calculates the min operations required to get n number of 'H'
 
     Return: Minimum number
     """
-    curr = 1 # current number of 'H's on the screen
-    clipboard = 0 # number of 'H's in the clipboard
-    steps = 0 # number of steps taken to reach n 'H's
+    curr = 1     # current number of 'H's on the screen
+    clipboard = 0     # number of 'H's in the clipboard
+    steps = 0     # number of steps taken to reach n 'H's
     # loop until curr equals n
     while curr < n:
         # if n is divisible by curr, perform copy and paste operations
